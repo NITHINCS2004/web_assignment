7650b938d9d5d738440eb30ae22faf9d140f9f94
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2024-11-12 20:38:32 +0530

    Initial commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+        <h2>Dashboard</h2>
+        <h2>Welcome, Persist Ventures</h2>
+    </div>
+  );
+}
+
+export default Header;
